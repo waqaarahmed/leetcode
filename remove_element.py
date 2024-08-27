da4a3aa89d5133c83e2130883ce1216815cdc035
Waqar Ahmed <ahmedwaqar309@gmail.com>
2024-08-27 23:42:28 +0500

    Update remove_element.py

--- remove_element.py
+++ remove_element.py
@@ -1,18 +1,15 @@
 """
- Remove Element
- Given an integer array nums and an integer val, remove all occurrences of val 
- in nums in-place. The order of the elements may be changed. Then return the number 
+ Remove Element:
+ Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the elements may be changed. Then return the number 
  of elements in nums which are not equal to val.
 
-Consider the number of elements in nums which are not equal to val be k, to get 
-accepted, you need to do the following things:
+Consider the number of elements in nums which are not equal to val be k, to get accepted, you need to do the following things:
 
-Change the array nums such that the first k elements of nums contain the elements 
-which are not equal to val. The remaining elements of nums are not important as well 
-as the size of nums.
+Change the array nums such that the first k elements of nums contain the elements which are not equal to val. 
+The remaining elements of nums are not important as well as the size of nums.
 Return k.
-Custom Judge:
 
+Custom Judge:
 The judge will test your solution with the following code:
 
 int[] nums = [...]; // Input array
